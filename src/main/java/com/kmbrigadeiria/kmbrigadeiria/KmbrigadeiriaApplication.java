eed45d8d04ba8669cb273f43bf29e0cca61a59a3
Pedro Marinho <pmaf@ic.ufal.br>
2023-06-26 14:22:13 -0300

    Initial project files.

---
+++ src/main/java/com/kmbrigadeiria/kmbrigadeiria/KmbrigadeiriaApplication.java
@@ -0,0 +1,13 @@
+package com.kmbrigadeiria.kmbrigadeiria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmbrigadeiriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmbrigadeiriaApplication.class, args);
+	}
+
+}
